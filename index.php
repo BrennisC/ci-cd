729cb9ec610388af263842f90c86286494e14ea8
brenniscastro <brennis.castro@unas.edu.pe>
2025-05-16 04:31:51 -0500

    name

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php
+
+$nombre = "Nombre de prueba";
+
+$listName = [
+    "pepe",
+    "brennis",
+    "benjaminn"
+]
 ?>
-<div>
-    hola mundo
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $nombre ?></title>
+</head>
+
+<body>
+    <ul>
+        <?php foreach ($listName as $name) { ?>
+            <li><?= $name ?></li>
+        <?php } ?>
+    </ul>
+</body>
+
+</html>
